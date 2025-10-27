c731eddc855233d46e2e64ec6c42bba1f031c04e
xenosign <xenosign@naver.com>
2025-10-27 16:55:15 +0900

    251027 jpa 참고 코드 추가

---
+++ src/main/java/com/tetz/jpa/member/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package com.tetz.jpa.member.repository;
+
+import com.tetz.jpa.member.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/com/tetz/jpa/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.tetz.jpa.member.repository;
+
+
+import com.tetz.jpa.member.entity.Member;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.findById(saveId);
+
+        // then
+        Assertions.assertEquals(member.getId(), findMember.getId());
+        Assertions.assertEquals(member.getUsername(), findMember.getUsername());
+        Assertions.assertEquals(findMember, member); // 같은 Transaction 에서는 같은 엔티티로 판단
+    }
+}
